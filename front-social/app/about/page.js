4bc0f548fcd98092a4b054a828044b62c6689239
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-04-03 21:18:11 +0530

    next js setup

---
+++ front-social/app/about/page.js
@@ -0,0 +1,14 @@
+// "use client";
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-pink-700">This is about</h1>
+      {/* {console.log("ABout baby")} */}
+    </div>
+  );
+};
+
+export default page;
---
+++ front-social/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-white font-bold'>No one told me</h1>
+  
+    </div>
+  )
+}
+
+export default page
