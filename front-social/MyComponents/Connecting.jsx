76661342da3544eaecc2884b4ae39311258914c6
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-04-11 21:29:04 +0530

    pushsing

---
+++ front-social/MyComponents/Connecting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Connecting = () => {
+  return (
+    <div><h1 className='text-4xl my-10 text-white/60 font-sans text-center'>Connect Your Social Media Accounts</h1></div>
+  )
+}
+
+export default Connecting
