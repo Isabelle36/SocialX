3066a66cab143b3c92c2864a8cda7f12eb9197c1
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-04-30 13:19:25 +0530

    fixed auth check login errors

--- front-social/app/api/auth/checking-login/route.js
+++ front-social/app/api/auth/checking-login/route.js
@@ -6,8 +6,12 @@ export async function GET(req) {
     ?.split("; ")
     .find((cookie) => cookie.startsWith("token="))
     ?.split("=")[1];
+  const refreshToken = cookies
+    ?.split("; ")
+    .find((cookie) => cookie.startsWith("refreshToken="))
+    ?.split("=")[1];
 
-  if (!token) {
+  if (!token && !refreshToken) {
     return new Response(JSON.stringify({ authenticated: false }), { status: 401 });
   }
 
@@ -16,6 +20,18 @@ export async function GET(req) {
     return new Response(JSON.stringify({ authenticated: true }), { status: 200 });
   } catch (error) {
     console.error("Token verification error:", error);
+    if(refreshToken)
+    {
+      try{
+        jwt.verify(refreshToken,`${process.env.REFRESH_TOKEN_SECRET}`);
+        return new Response(JSON.stringify({authenticated:true}),{status:200})
+      }
+      catch(err)
+      {
+        console.error("Refresh token verification error");
+        return new Response(JSON.stringify({authenticated:false}),{status:401});
+      }
+    }
     return new Response(JSON.stringify({ authenticated: false }), { status: 401 });
   }
 }
