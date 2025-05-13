8931765bc15560c73a1425c648a1eb9f731a1c7f
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-05-13 13:39:40 +0530

    single post and video and just image carousels r working

--- front-social/app/api/scheduling-post/route.js
+++ front-social/app/api/scheduling-post/route.js
@@ -26,12 +26,16 @@ export async function POST(req) {
       mediaUrlKey = "video_url";
       agendaJob = "publishSingleVideo";
     } else if (
-      (imageUrls.length === 1 && imageUrls[0].endsWith(".jpg")) ||
-      imageUrls[0].endsWith(".jpeg")
+      imageUrls.length === 1 &&
+      (imageUrls[0].endsWith(".jpg") || imageUrls[0].endsWith(".jpeg"))
     ) {
       mediaType = "image";
       mediaUrlKey = "image_url";
       agendaJob = "publishSingleImage";
+    } else if (imageUrls.length > 1) {
+      mediaType = "carousel";
+      mediaUrlKey = "carouselsContent_url";
+      agendaJob = "publishCarousel";
     } else {
       return new Response(
         JSON.stringify({ error: "Unsupported media type or multiple URLs" }),
@@ -39,28 +43,51 @@ export async function POST(req) {
       );
     }
 
-    const post = await db.collection("scheduledPosts").insertOne({
-      [mediaUrlKey]: imageUrls[0],
-      media_type: mediaType === "video" ? "REELS" : "IMAGE",
-      caption,
-      scheduledTime: new Date(scheduledTime),
-      igUserId,
-      accessToken,
-      status: "pending",
-    });
+    if (imageUrls.length === 1) {
+      const post = await db.collection("scheduledPosts").insertOne({
+        [mediaUrlKey]: imageUrls[0],
+        media_type: mediaType === "video" ? "REELS" : "IMAGE",
+        caption,
+        scheduledTime: new Date(scheduledTime),
+        igUserId,
+        accessToken,
+        status: "pending",
+      });
 
-    await agenda.start();
-    await agenda.schedule(new Date(scheduledTime), agendaJob, {
-      postId: post.insertedId,
-    });
-
-    return new Response(
-      JSON.stringify({
-        message: "Post scheduled successfully",
+      await agenda.start();
+      await agenda.schedule(new Date(scheduledTime), agendaJob, {
         postId: post.insertedId,
-      }),
-      { status: 200 }
-    );
+      });
+      return new Response(
+        JSON.stringify({
+          message: "Post scheduled successfully",
+          postId: post.insertedId,
+        }),
+        { status: 200 }
+      );
+    } else if (imageUrls.length > 1) {
+      const post = await db.collection("scheduledPosts").insertOne({
+        children: imageUrls,
+        media_type: "carousel",
+        caption,
+        scheduledTime: new Date(scheduledTime),
+        igUserId,
+        accessToken,
+        status: "pending",
+      });
+
+      await agenda.start();
+      await agenda.schedule(new Date(scheduledTime), agendaJob, {
+        postId: post.insertedId,
+      });
+      return new Response(
+        JSON.stringify({
+          message: "Post scheduled successfully",
+          postId: post.insertedId,
+        }),
+        { status: 200 }
+      );
+    }
   } catch (err) {
     console.error("Error scheduling post", err);
     return new Response(JSON.stringify({ error: "Internal server error" }), {
@@ -158,7 +185,6 @@ agenda.define("publishSingleImage", async (job) => {
   }
 });
 
-
 agenda.define("publishSingleVideo", async (job) => {
   const { postId } = job.attrs.data;
 
@@ -173,7 +199,6 @@ agenda.define("publishSingleVideo", async (job) => {
       return;
     }
 
-   
     if (post.status === "Published") {
       console.log("Post already published:", postId);
       return;
@@ -185,7 +210,7 @@ agenda.define("publishSingleVideo", async (job) => {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({
-          video_url: post.video_url,
+          video_url: post.video_url[0],
           caption: post.caption,
           access_token: post.accessToken,
           media_type: "REELS",
@@ -197,10 +222,12 @@ agenda.define("publishSingleVideo", async (job) => {
 
     if (creationResponse.error) {
       console.error("Error creating media:", creationResponse.error.message);
-      await db.collection("scheduledPosts").updateOne(
-        { _id: postId },
-        { $set: { status: "failed", error: creationResponse.error.message } }
-      );
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          { $set: { status: "failed", error: creationResponse.error.message } }
+        );
       return;
     }
 
@@ -211,8 +238,10 @@ agenda.define("publishSingleVideo", async (job) => {
     let mediaStatus = "IN_PROGRESS";
 
     while (mediaStatus === "IN_PROGRESS" && retries < maxRetries) {
-      console.log(`Media not ready, retrying (${retries + 1}/${maxRetries})...`);
-      await new Promise((resolved) => setTimeout(resolved, 5000)); 
+      console.log(
+        `Media not ready, retrying (${retries + 1}/${maxRetries})...`
+      );
+      await new Promise((resolved) => setTimeout(resolved, 5000));
 
       const statusCheck = await fetch(
         `https://graph.facebook.com/v22.0/${creationId}?fields=status_code&access_token=${post.accessToken}`
@@ -221,10 +250,12 @@ agenda.define("publishSingleVideo", async (job) => {
 
       if (statusResponse.error) {
         console.error("Error checking status:", statusResponse.error.message);
-        await db.collection("scheduledPosts").updateOne(
-          { _id: postId },
-          { $set: { status: "failed", error: statusResponse.error.message } }
-        );
+        await db
+          .collection("scheduledPosts")
+          .updateOne(
+            { _id: postId },
+            { $set: { status: "failed", error: statusResponse.error.message } }
+          );
         return;
       }
 
@@ -234,10 +265,14 @@ agenda.define("publishSingleVideo", async (job) => {
 
     if (mediaStatus !== "FINISHED") {
       console.error("Media is not ready for publishing after retries.");
-      await db.collection("scheduledPosts").updateOne(
-        { _id: postId },
-        { $set: { status: "failed", error: "Media not ready after retries." } }
-      );
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          {
+            $set: { status: "failed", error: "Media not ready after retries." },
+          }
+        );
       return;
     }
 
@@ -257,24 +292,273 @@ agenda.define("publishSingleVideo", async (job) => {
 
     if (publishResponse.error) {
       console.error("Error publishing post:", publishResponse.error.message);
-      await db.collection("scheduledPosts").updateOne(
-        { _id: postId },
-        { $set: { status: "failed", error: publishResponse.error.message } }
-      );
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          { $set: { status: "failed", error: publishResponse.error.message } }
+        );
       return;
     }
 
-    await db.collection("scheduledPosts").updateOne(
-      { _id: postId },
-      { $set: { status: "Published", PublishedAt: new Date() } }
-    );
+    await db
+      .collection("scheduledPosts")
+      .updateOne(
+        { _id: postId },
+        { $set: { status: "Published", PublishedAt: new Date() } }
+      );
 
     console.log("Video post published successfully:", publishResponse.id);
   } catch (err) {
     console.error("Error publishing video post:", err.message);
-    await db.collection("scheduledPosts").updateOne(
-      { _id: postId },
-      { $set: { status: "failed", error: err.message } }
+    await db
+      .collection("scheduledPosts")
+      .updateOne(
+        { _id: postId },
+        { $set: { status: "failed", error: err.message } }
+      );
+  }
+});
+
+agenda.define("publishCarousel", async (job) => {
+  const { postId } = job.attrs.data;
+
+  const client = await clientPromise;
+  const db = client.db("socialX");
+
+  try {
+    const post = await db.collection("scheduledPosts").findOne({ _id: postId });
+
+    if (!post) {
+      console.error("Post not found:", postId);
+      return;
+    }
+
+    if (post.status === "Published") {
+      console.log("Post already published:", postId);
+      return;
+    }
+
+    const childrenIds = [];
+    const maxRetries = 20;
+
+    for (const element of post.children) {
+      const isImage = element.endsWith(".jpg") || element.endsWith(".jpeg");
+      if (isImage) {
+        const creatingCreationId = await fetch(
+          `https://graph.facebook.com/v22.0/${post.igUserId}/media`,
+          {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+              image_url: element,
+              is_carousel_item: true,
+              access_token: post.accessToken,
+            }),
+          }
+        );
+
+        const respOfCreationId = await creatingCreationId.json();
+        console.log("starting to create creation id..");
+        if (respOfCreationId.error) {
+          console.error(
+            "Error creating media:",
+            respOfCreationId.error.message
+          );
+          await db
+            .collection("scheduledPosts")
+            .updateOne(
+              { _id: postId },
+              {
+                $set: {
+                  status: "failed",
+                  error: respOfCreationId.error.message,
+                },
+              }
+            );
+          return;
+        }
+
+        const creationId = respOfCreationId.id;
+        console.log("Creation ID:", creationId);
+        let retries = 0;
+        let mediaStatus = "IN_PROGRESS";
+
+        while (mediaStatus === "IN_PROGRESS" && retries < maxRetries) {
+          console.log(
+            `Retry ${retries + 1}/${maxRetries} - Media Status: ${mediaStatus}`
+          );
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+
+          const statusCheck = await fetch(
+            `https://graph.facebook.com/v22.0/${creationId}?fields=status_code&access_token=${post.accessToken}`
+          );
+          const statusResponse = await statusCheck.json();
+
+          if (statusResponse.error) {
+            console.error(
+              "Error checking status:",
+              statusResponse.error.message
+            );
+            await db
+              .collection("scheduledPosts")
+              .updateOne(
+                { _id: postId },
+                {
+                  $set: {
+                    status: "failed",
+                    error: statusResponse.error.message,
+                  },
+                }
+              );
+            return;
+          }
+
+          mediaStatus = statusResponse.status_code;
+          retries++;
+        }
+
+        if (mediaStatus !== "FINISHED") {
+          console.error("Media is not ready for publishing after retries.");
+          await db
+            .collection("scheduledPosts")
+            .updateOne(
+              { _id: postId },
+              {
+                $set: {
+                  status: "failed",
+                  error: "Media not ready after retries.",
+                },
+              }
+            );
+          return;
+        }
+
+        childrenIds.push(creationId);
+      }
+      else{
+        console.log("Not an image URL, skipping:", element);
+      }
+    }
+
+    const creatingCreationId = await fetch(
+      `https://graph.facebook.com/v22.0/${post.igUserId}/media`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          media_type: "CAROUSEL",
+          children: childrenIds.join(","),
+          caption: post.caption,
+          access_token: post.accessToken,
+        }),
+      }
     );
+
+    const creationResponse = await creatingCreationId.json();
+    console.log("the resp of carousel media api call", creationResponse);
+
+    if (creationResponse.error) {
+      console.error("Error creating media:", creationResponse.error.message);
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          { $set: { status: "failed", error: creationResponse.error.message } }
+        );
+      return;
+    }
+
+    const creationIdCarousel = creationResponse.id;
+    console.log("Final children IDs for carousel:", childrenIds);
+    console.log("Creation ID of the carousel :", creationIdCarousel);
+
+    let retries = 0;
+    let mediaStatus = "IN_PROGRESS";
+
+    while (mediaStatus === "IN_PROGRESS" && retries < maxRetries) {
+      console.log(
+        `Media not ready, retrying (${retries + 1}/${maxRetries})...`
+      );
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      const statusCheck = await fetch(
+        `https://graph.facebook.com/v22.0/${creationIdCarousel}?fields=status_code&access_token=${post.accessToken}`
+      );
+      const statusResponse = await statusCheck.json();
+      console.log(statusResponse);
+      console.log(
+        "the status of the carousel media",
+        statusResponse.status_code
+      );
+      if (statusResponse.error) {
+        console.error("Error checking status:", statusResponse.error.message);
+        await db
+          .collection("scheduledPosts")
+          .updateOne(
+            { _id: postId },
+            { $set: { status: "failed", error: statusResponse.error.message } }
+          );
+        return;
+      }
+
+      mediaStatus = statusResponse.status_code;
+      retries++;
+    }
+
+    if (mediaStatus !== "FINISHED") {
+      console.error("Media is not ready for publishing after retries.");
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          {
+            $set: { status: "failed", error: "Media not ready after retries." },
+          }
+        );
+      return;
+    }
+
+    const publishThePost = await fetch(
+      `https://graph.facebook.com/v22.0/${post.igUserId}/media_publish`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          creation_id: creationIdCarousel,
+          access_token: post.accessToken,
+        }),
+      }
+    );
+
+    const publishResponse = await publishThePost.json();
+
+    if (publishResponse.error) {
+      console.error("Error publishing post:", publishResponse.error.message);
+      await db
+        .collection("scheduledPosts")
+        .updateOne(
+          { _id: postId },
+          { $set: { status: "failed", error: publishResponse.error.message } }
+        );
+      return;
+    }
+
+    await db
+      .collection("scheduledPosts")
+      .updateOne(
+        { _id: postId },
+        { $set: { status: "Published", PublishedAt: new Date() } }
+      );
+
+    console.log("Carousel post published successfully:", publishResponse.id);
+  } catch (err) {
+    console.error("Error publishing Carousel post:", err.message);
+    await db
+      .collection("scheduledPosts")
+      .updateOne(
+        { _id: postId },
+        { $set: { status: "failed", error: err.message } }
+      );
   }
 });
